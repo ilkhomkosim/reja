125adf54501f4d70f782a0a32ea0e1b894362c54
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-02-04 23:13:51 +0900

    harid

--- server.js
+++ server.js
@@ -20,16 +20,16 @@ app.set("view engine", "ejs");
 // ejsda backendda front end yasaymiz
 
 //4 Routing code
-
-
-app.get("/hello", function(req, res) {
-    res.end("<h1>Hello World by IlkhomKosimov</h1>");
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"});
 });
-app.get("/gift", function(req, res) {
-    res.end("<h1>Siz sovg'alar bo'limidasiz</h1>");
+app.get('/', function(req, res){
+    res.render("harid.ejs");
 });
+
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function(){
     console.log(`The server is qrunning succesfully on port: ${PORT} `);
-});
+})
