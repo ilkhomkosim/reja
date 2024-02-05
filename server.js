61f9d9edce76093b1e63d5629560195ef4779e6f
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-02-05 18:10:54 +0900

    feat: developer authot router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ console.log("Web serverni boshlash");
 const express = require('express')
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) =>{
+    if(err) {
+        console.log("ERROR", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
+
 //1 Kirish codelari
 
 app.use(express.static("public"));
@@ -21,15 +32,17 @@ app.set("view engine", "ejs");
 
 //4 Routing code
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
+    // console.log(req.body);
+    // res.json({test: "success"});
 });
 app.get('/', function(req, res){
     res.render("harid.ejs");
 });
-
+app.get("/author", (req, res) =>{
+    res.render("author", {user: user})
+});
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function(){
     console.log(`The server is qrunning succesfully on port: ${PORT} `);
-})
+});
