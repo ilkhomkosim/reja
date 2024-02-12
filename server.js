790f0a981030cca85d15b4603f1518d9bd741c6c
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-02-12 19:24:16 +0900

    feat: CRUD operatsiyalari mongodb

--- server.js
+++ server.js
@@ -1,49 +1,89 @@
+// const http = require("http");
+// const mongodb = require("mongodb");
+// const app = require("./app");
+// let db;
+// const connectionString =
+// "mongodb+srv://uilyam:uppE3oXwT8FTnKOk@cluster0.egc6jxs.mongodb.net/reja"
+
+
+// mongodb.connect(
+//     connectionString, 
+//     {
+//     useNewUrlParser:true,
+//     useUnifiedTopology: true,
+//     }, 
+//     (err, client) => {
+//     if(err) console.log("ERROR on connection MongoDb");
+//     else{
+//         console.log("MongoDB connection succeed");
+//         const db = client.db();
+//         const server = http.createServer(app);
+//         let PORT = 3000;
+//         server.listen(PORT, function() {
+//             console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
+//             );
+//         });
+//     }
+// }
+// );
+
+// // const http = require("http");
+// // const mongodb = require("mongodb");
+
+// // // Placeholder for the "app" module (replace this with your actual implementation)
+// // const app = require("./app");
+
+// // const connectionString = "mongodb+srv://uilyam:uppE3oXwT8FTnKOk@cluster0.egc6jxs.mongodb.net/reja";
+
+// // mongodb.connect(
+// //     connectionString,
+// //     {
+// //         useNewUrlParser: true,
+// //         useUnifiedTopology: true,
+// //     },
+// //     (err, client) => {
+// //         if (err) {
+// //             console.error("ERROR on connection MongoDB:", err);
+// //         } else {
+// //             console.log("MongoDB connection succeed");
+// //             module.exports = client;
+// //             const server = http.createServer(app);
+
+// //             const PORT = 3000;
+// //             server.listen(PORT, () => {
+// //                 console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+// //             });
+// //         }
+// //     }
+// // );
+
+
 console.log("Web serverni boshlash");
-const express = require('express')
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) =>{
-    if(err) {
-        console.log("ERROR", err);
-    } else {
-        user = JSON.parse(data)
+// mongodb connection
+let db;
+const connectionString =
+    "mongodb+srv://uilyam:uppE3oXwT8FTnKOk@cluster0.egc6jxs.mongodb.net/reja";
+mongodb.connect(
+    connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err, client) => {
+        if (err) console.log("Error on connection to mongoDB:", err);
+        else {
+            console.log("MongoDB connection succeed");
+            module.exports = client;
+
+            const app = require("./app");
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function () {
+                console.log(`
+                The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+            });
+        }
     }
-});
-
-//1 Kirish codelari
-
-app.use(express.static("public"));
-app.use(express.json());
-// kirib kelayotkan json formatdagi datani objectga o'girib beradi
-app.use(express.urlencoded({extended: true}));
-// bu kod htmldan tradidional forum request  bron narsani request qilsak bizni express forumimiz qabul qilib oladi
-
-
-//2 sessions code
-
-//3 Views code
-// BSSRsa front end yasash
-app.set("views", "views");
-app.set("view engine", "ejs");
-// ejsda backendda front end yasaymiz
-
-//4 Routing code
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
-});
-app.get('/', function(req, res){
-    res.render("harid.ejs");
-});
-app.get("/author", (req, res) =>{
-    res.render("author", {user: user})
-});
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function(){
-    console.log(`The server is qrunning succesfully on port: ${PORT} `);
-});
-
+);
