4fb84fb24fd19d3b1bcb9c062d0f45d6ae8fd7b5
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-02-14 16:52:37 +0900

    fix: CRUD operations

---
+++ shop.js
@@ -0,0 +1,4 @@
+const shopp = require("./train")
+
+const shopp = new Shop(5, 4, 8);
+shop.qoldiq();
--- train.js
+++ train.js
@@ -1,13 +1,43 @@
-// Tasks
-function count_digits(str) {
-  let count = 0;
-  for (let a of str) {
-    if (a > "3" && a <= "7") {
-      count++;
+// D-TASK: 
+
+/*Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot
+pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va 
+biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 
+4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4)
+& shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+*/
+
+const moment = require('moment');
+
+const now = moment();
+
+console.log("Current Date and Time:", now.format("YYYY-MM-DD HH:mm:ss"));
+class Shop {
+    //state
+
+
+    constructor(non, cola, lagmon) {
+        this.non = non;
+        this.cola = cola;
+        this.lagmon = lagmon;
     }
-  }
-  return count;
+
+    // methods
+    qoldiq() {
+        console.log(now.format(" HH:mm"), "da");
+        console.log( ` ${this.non} ta non , ${this.lagmon} ta lag'mon va ${this.cola} ta cola mavjud!`);
+    }
+    sotish() {
+        console.log(`${this.non} ta , ${this.lagmon} ta , ${this.cola} ta sotildi`);
+    }
+
+    qabul() {
+        console.log(`${this.non} ta , ${this.lagmon} ta , ${this.cola} ta qabul qilindi`);
+    }
 }
 
-const natija = count_digits("ah98mad7al3ie1v");
-console.log(natija);
+
+const shop = new Shop(5, 4, 8);
+shop.qoldiq();
